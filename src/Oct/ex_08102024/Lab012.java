d7a937eece22e2f0a18502cdeff0fecb65c5c0e7
Yash Dhurve <yash43075@gmail.com>
2024-10-26 15:17:51 +0530

    26th Oct 2024 extra programs

---
+++ src/Oct/ex_08102024/Lab012.java
@@ -0,0 +1,15 @@
+package Oct.ex_08102024;
+
+public class Lab012 {
+    public static void main(String[] args) {
+        // int age=123;
+        // int _ = 12;
+        // int $ = 12;
+        // int yash = 25;
+        //int cal= "yash" -> invalid
+        //int cal= c -> invalid
+        int _1 = 1;
+        _1=567;
+        System.out.println(_1);
+    }
+}
